3bc2d2775bf4cb75e989f4a1cbbb5c4aaf954d61
Juan Pablo Prado <juan@juanpabloprado.com>
2017-09-29 12:27:30 -0500

    Update comic book model

--- src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -9,5 +9,23 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        // series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                                  .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
